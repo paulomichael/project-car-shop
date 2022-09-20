6b578fa0d9fcdf55f2044fc144153411844c87f4
Paulo Michael <paulo.michael@gmail.com>
2022-09-19 21:39:23 -0300

    feat: req01 - creates `IModel` interface + `MongoModel.ts`

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,8 @@
+export interface IModel<T> {
+  // create(arg0: {T}): Promise<T>;
+  create(obj: T): Promise<T>;
+  read(): Promise<T[]>;
+  readOne(_id: string): Promise<T|null>;
+  update(_id: string, obj: T): Promise<T|null>;
+  delete(_id: string): Promise<T|null>;
+}
---
+++ src/models/MongoModel.ts
@@ -0,0 +1,38 @@
+// ./src/models/MongoModel.ts
+import { isValidObjectId, Model } from 'mongoose'
+import {IModel} from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  // atributos...
+  // métodos...
+  protected _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj:T):Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read():Promise<T[]> {
+    return this._model.find();
+  }
+  
+  public async readOne(_id: string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async update(_id: string, obj: T):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOneAndUpdate({ _id, obj });
+  }
+
+  public async delete(_id: string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findByIdAndDelete({ _id });
+  }
+}
+
+export default MongoModel;
