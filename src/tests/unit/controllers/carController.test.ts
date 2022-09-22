ffb4fa8a38baa618fe8348041920f930e5a86879
Paulo Michael <paulo.michael@gmail.com>
2022-09-22 19:16:51 -0300

    fix: car controller create and readOne tested

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -12,56 +12,60 @@ describe('Car Controller', () => {
   const carModel = new CarModel()
   const carService = new CarService(carModel);
   const carController = new CarController(carService);
-  // fazemos o cast de um objeto para um `Request` pois nosso controller só vai aceitar um objeto do tipo `Request` como primeiro parâmetro
+
   const req = {} as Request; 
-  // o mesmo acontece com o segundo parâmetro
   const res = {} as Response;
 
   const carMock = {
-    model: 'Volkswagen Beetle Type 1',
-    year: 1977,
-    color: 'beige',
-    buyValue: 20000,
-    seatsQty: 4,
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
     doorsQty: 2
   };
 
-  before(async() => {
-    sinon.stub(carService, 'create').resolves(carMock);
-    // sinon.stub(carService, 'read').resolves(carMock);
-    // sinon.stub(carService, 'readOne').resolves([carMock]);
-    sinon.stub(carService, 'update').resolves(carMock);
-    sinon.stub(carService, 'delete').resolves(null);
-    sinon.stub(carService, 'create').resolves(carMock);
+  // before(async() => {
+  //   sinon.stub(carService, 'create').resolves(carMock);
+  //   // sinon.stub(carService, 'read').resolves(carMock);
+  //   // sinon.stub(carService, 'readOne').resolves([carMock]);
+  //   sinon.stub(carService, 'update').resolves(carMock);
+  //   sinon.stub(carService, 'delete').resolves(null);
+  //   sinon.stub(carService, 'create').resolves(carMock);
 
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
-  });
+  //   res.status = sinon.stub().returns(res);
+  //   res.json = sinon.stub().returns(res);
+  // });
 
-  after(() => {
-    sinon.restore()
-  })
+  // after(() => {
+  //   sinon.restore()
+  // })
 
   describe('Create Car', () => {
     it('Success', async () => {
+      sinon.stub(carService, 'create').resolves(carMock);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
       req.body = carMock;
       await carController.create(req, res);
-      // o cast de `res.status` é feito pois `res` foi criado como do tipo `Resquest` 
-      // e agora, que queremos validar com o que foi chamado, precisar ser tratado como um `sinon.SinonStub`
       expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
-      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+      // expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+      sinon.restore()
+
     });
   });
 
-  // describe('ReadOne Car', () => {
-  //   it('Success', async () => {
-  //     // como fizemos o dublê da service o valor do `req.params.id` não vai chegar na model
-  //     // logo ele só precisa ser um string e existir
-  //     req.params = { id: carMock._id };
-  //     await carController.readOne(req, res);
+  describe('ReadOne Car', () => {
+    it('Success', async () => {
+      sinon.stub(carService, 'readOne').resolves(carMock);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      req.params = { id: 'identifier' };
+      await carController.readOne(req, res);
 
-  //     expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
-  //     expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
-  //   });
-  // });
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      // expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+      sinon.restore()
+    });
+  });
 });
